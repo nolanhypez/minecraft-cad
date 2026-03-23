13526b9a9b40d3d0d776ea3b43b1fa6e2662b7be
Nolan <nolandortonbusiness@gmail.com>
2026-03-22 21:11:13 -0400

    Create CADCommand.java for handling all CAD commands

---
+++ src/main/java/com/nolanhypez/cad/command/CADCommand.java
@@ -0,0 +1,9 @@
+package com.nolanhypez.cad.command;
+
+public class CADCommand {
+    // Handle all CAD commands here
+
+    public void executeCommand(String command) {
+        // Logic for executing the CAD commands
+    }
+}
