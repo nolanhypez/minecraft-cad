70bbdc50b7006e3451958a3ef440eb63219b3d42
Nolan <nolandortonbusiness@gmail.com>
2026-03-23 02:22:52 -0400

    Update package declaration in MinecraftCAD.java

--- src/main/java/com/nolanhypez/cad/MinecraftCAD.java
+++ src/main/java/com/nolanhypez/cad/MinecraftCAD.java
@@ -1,6 +1,1 @@
-public class MinecraftCAD {
-    // Main class for the MinecraftCAD plugin
-    public static void main(String[] args) {
-        // Plugin logic here
-    }
-}
+package com.nolanhypez.cad;\n
